80c749bfa312d673be04b7520a40c4459969379a
sr-mobileappdev <coltonanglin119@gmail.com>
2019-10-13 23:50:46 +0200

    Build Android apk

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,67 +1,73 @@
-import React from 'react';
-import {
-  Icon, Dimensions
-} from 'react-native';
-import { createStackNavigator, createAppContainer, createDrawerNavigator } from 'react-navigation';
+import React from "react";
+import { Icon, Dimensions } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createDrawerNavigator } from "react-navigation-drawer";
 
-import Start from '../screens/StartScreen';
-import CreateAccount from '../screens/CreateAccountScreen';
-import Home from '../screens/HomeScreen';
-import Account from '../screens/AccountScreen';
-import Detail from '../screens/DetailScreen';
-import Other from '../screens/OtherScreen';
-import Speaker from '../screens/SpeakerScreen';
-import Keynote from '../screens/KeynoteScreen';
-import Agenda from '../screens/AgendaScreen';
-import Venue from '../screens/VenueScreen';
-import Social from '../screens/SocialScreen';
-import ProgramAtGlance from '../screens/ProgramAtGlanceScreen';
-import MySchedule from '../screens/MyScheduleScreen';
-import SponsorExhibitors from '../screens/SponsorExhibitorsScreen';
-import Terms from '../screens/TermsScreen';
-import SideMenu from '../components/SideMenu';
+import Start from "../screens/StartScreen";
+import CreateAccount from "../screens/CreateAccountScreen";
+import Home from "../screens/HomeScreen";
+import Account from "../screens/AccountScreen";
+import Detail from "../screens/DetailScreen";
+import Other from "../screens/OtherScreen";
+import Speaker from "../screens/SpeakerScreen";
+import Keynote from "../screens/KeynoteScreen";
+import Agenda from "../screens/AgendaScreen";
+import Venue from "../screens/VenueScreen";
+import Social from "../screens/SocialScreen";
+import ProgramAtGlance from "../screens/ProgramAtGlanceScreen";
+import MySchedule from "../screens/MyScheduleScreen";
+import SponsorExhibitors from "../screens/SponsorExhibitorsScreen";
+import Terms from "../screens/TermsScreen";
+import SideMenu from "../components/SideMenu";
 
-const drawNav = createDrawerNavigator({
-  ProgramAtGlance: { screen: ProgramAtGlance },
-  Speaker: { screen: Speaker },
-  Keynote: { screen: Keynote },
-  MySchedule: { screen: MySchedule },
-  Agenda: { screen: Agenda },
-  Venue: { screen: Venue },
-  Social: { screen: Social },
-  Other: { screen: Other },
-  SponsorExhibitors: { screen: SponsorExhibitors },
-  Account: { screen: Account},  
-},{
-    headerMode: 'none',
+const drawNav = createDrawerNavigator(
+  {
+    ProgramAtGlance: { screen: ProgramAtGlance },
+    Speaker: { screen: Speaker },
+    Keynote: { screen: Keynote },
+    MySchedule: { screen: MySchedule },
+    Agenda: { screen: Agenda },
+    Venue: { screen: Venue },
+    Social: { screen: Social },
+    Other: { screen: Other },
+    SponsorExhibitors: { screen: SponsorExhibitors },
+    Account: { screen: Account }
+  },
+  {
+    headerMode: "none",
     navigationOptions: {
-      headerVisible: false,
+      headerVisible: false
     },
-    drawerType: 'slide',
+    drawerType: "slide",
     contentComponent: SideMenu,
-    drawerWidth: Dimensions.get('window').width - 50,  
-});
+    drawerWidth: Dimensions.get("window").width - 50
+  }
+);
 
-
-const RootStack = createStackNavigator({
+const RootStack = createStackNavigator(
+  {
     Start: { screen: Start },
-    CreateAccount: { screen: CreateAccount},
-    Home: { screen: drawNav},
+    CreateAccount: { screen: CreateAccount },
+    Home: { screen: drawNav },
     Home_noTransition: { screen: drawNav },
     Terms: { screen: Terms },
-    Detail: { screen: Detail},
-}, {
-  headerMode: 'none',
-  navigationOptions: {
-    headerVisible: false,
+    Detail: { screen: Detail }
   },
-	transitionConfig: (sceneProps) => ({
+  {
+    headerMode: "none",
+    navigationOptions: {
+      headerVisible: false
+    },
+    transitionConfig: sceneProps => ({
       transitionSpec: {
-        duration: sceneProps.scene.route.routeName.endsWith('_noTransition') ? 0 : 260,
-      },
-    }),
-});
+        duration: sceneProps.scene.route.routeName.endsWith("_noTransition")
+          ? 0
+          : 260
+      }
+    })
+  }
+);
 
 const AppNavigator = createAppContainer(RootStack);
 export default AppNavigator;
-
